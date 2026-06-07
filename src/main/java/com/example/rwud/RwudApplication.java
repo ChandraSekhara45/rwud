2c08140e10d2fc78b2189555b834be64236891c8
ChandraSekhara45 <rvcsraju@gmail.com>
2026-06-07 19:39:37 +0530

    Initilised a SpringBoot Project

---
+++ src/main/java/com/example/rwud/RwudApplication.java
@@ -0,0 +1,13 @@
+package com.example.rwud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rwud/RwudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rwud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RwudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
